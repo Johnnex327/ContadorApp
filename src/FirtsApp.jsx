93272058ba67f18ffc9411e8fba4a1c7059ace67
jhon <jhon.naib327@gmail.com>
2024-12-27 18:15:05 -0500

    first commit

---
+++ src/FirtsApp.jsx
@@ -0,0 +1,16 @@
+
+const newMessage = {
+    message: '',
+    titulo: ''
+}
+
+
+
+export const FirstApp = () => {
+  return (
+    <>
+      <h1>{newMessage}</h1>
+      <p>Soy un subtitulo</p>
+    </>
+  );
+};
---
+++ src/HelloWorldApp.jsx
@@ -0,0 +1,12 @@
+
+
+
+export const HelloWorldApp = () => {
+    return (
+        <h1> 
+            Hello World App
+        </h1>
+    )
+}
+
+
